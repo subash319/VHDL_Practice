e2ca87e39c9655e876d570d69376f7b04419d999
subash319 <subas319@gmail.com>
2020-09-30 16:17:35 -0400

    Lab 2

--- TEMPMUX.vhd
+++ TEMPMUX.vhd
@@ -1,25 +1,45 @@
 -- This design 7*1 Mux, selects CURRENT_TEMP if DISPLAY_SELECT is 1
 -- else it will be DESIRED_TEMP
+--Add 2 new outputs to our entity ,  A_C_ON , and FURNACE_ON.
+--Add logic for determining when the A_C_ON and FURNACE_ON are '1', and when they are '0'.
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 entity TEMPMUX is
 	port(
 	    	CURRENT_TEMP	: in bit_vector(6 downto 0);	
 	    	DESIRED_TEMP	: in bit_vector(6 downto 0);	
-	    	DISPLAY_SELECT	: in bit;	
+	    	DISPLAY_SELECT	: in bit;
+		COOL		: in bit;	
+		HEAT		: in bit;	
+		AC_ON		: out bit;	
+		FURNACE_ON	: out bit;	
 	    	TEMP_DISPLAY	: out bit_vector(6 downto 0));
 end TEMPMUX;
 
 architecture BEHAV of TEMPMUX is
 begin
 
-process(CURRENT_TEMP,DESIRED_TEMP,DISPLAY_SELECT)
+process(CURRENT_TEMP,DESIRED_TEMP,DISPLAY_SELECT,COOL,HEAT)
 begin
 	if DISPLAY_SELECT = '1' then
 		TEMP_DISPLAY <= CURRENT_TEMP;
 	else
 		TEMP_DISPLAY <= DESIRED_TEMP;
 	end if;
+
+	if COOL = '1' and CURRENT_TEMP > DESIRED_TEMP then
+		AC_ON	<=	'1';
+	else
+		AC_ON	<=	'0';
+	end if;
+
+	if HEAT = '1' and CURRENT_TEMP < DESIRED_TEMP then
+		FURNACE_ON	<=	'1';
+	else
+		FURNACE_ON	<=	'0';
+	end if;
+
 end process;
 
 end BEHAV;
--- T_TEMPMUX.vhd
+++ T_TEMPMUX.vhd
@@ -6,32 +6,60 @@ end T_TEMPMUX;
 architecture TEST of T_TEMPMUX is
 component TEMPMUX is
 	port(
-	    CURRENT_TEMP	: in bit_vector(6 downto 0);	
-	    DESIRED_TEMP	: in bit_vector(6 downto 0);	
-	    DISPLAY_SELECT	: in bit;	
-	    TEMP_DISPLAY	: out bit_vector(6 downto 0));
+	    	CURRENT_TEMP	: in bit_vector(6 downto 0);	
+	    	DESIRED_TEMP	: in bit_vector(6 downto 0);	
+	    	DISPLAY_SELECT	: in bit;
+		COOL		: in bit;	
+		HEAT		: in bit;	
+		AC_ON		: out bit;	
+		FURNACE_ON	: out bit;	
+	    	TEMP_DISPLAY	: out bit_vector(6 downto 0));
 end component;
 
 signal CURRENT_TEMP, DESIRED_TEMP : bit_vector(6 downto 0);
 signal DISPLAY_SELECT 		  : bit;
+signal COOL	 		  : bit;
+signal HEAT 			  : bit;
 signal TEMP_DISPLAY 		  : bit_vector(6 downto 0);
+signal AC_ON 			  : bit;
+signal FURNACE_ON 		  : bit;
+
 begin
 --Instantiaton of the design, left one's are the pins(Formal pins)
 --This is called explicit Instantation where it is position independent, always preferrable	
 UUT: TEMPMUX port map (CURRENT_TEMP	=>	CURRENT_TEMP,				
                       DESIRED_TEMP	=>	DESIRED_TEMP,	
-                      DISPLAY_SELECT	=>	DISPLAY_SELECT,	
+                      DISPLAY_SELECT	=>	DISPLAY_SELECT,
+		      COOL		=>	COOL,			
+		      HEAT		=>	HEAT,		
+		      AC_ON		=>	AC_ON,		
+		      FURNACE_ON	=>	FURNACE_ON,	
 	              TEMP_DISPLAY 	=>	TEMP_DISPLAY); 
 process 
 begin
 	CURRENT_TEMP	<=	"1010101";	
 	DESIRED_TEMP	<=	"1111000";
         DISPLAY_SELECT	<=	'0';
+	HEAT		<= 	'0';
+	COOL		<=	'0';
 	wait for 10 ns;
         DISPLAY_SELECT	<=	'1';
+	HEAT		<=	'1';
+	COOL		<=	'1';
 	wait for 10 ns;
+	CURRENT_TEMP	<=	"1111111";	
+	DESIRED_TEMP	<=	"1111000";
+        DISPLAY_SELECT	<=	'0';
+	HEAT		<= 	'0';
+	COOL		<=	'0';
+	wait for 10 ns;
+	COOL		<=	'1';
+	wait for 10 ns;
+	CURRENT_TEMP	<=	"1111100";
+	DISPLAY_SELECT	<=	'0';
+	wait for 10 ns;
+	CURRENT_TEMP	<=	"1111111";
 	wait;
-
 end process;
 	
 end TEST;	      
